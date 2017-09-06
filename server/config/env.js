0d6e90d36815f705bd9408ec4070d48a5d13bc1c
Tyler Rhinehart <rhinehart22@gmail.com>
2017-09-06 16:17:40 -0600

    added cloudinary api keys

--- server/config/env.js
+++ server/config/env.js
@@ -7,14 +7,20 @@ let env = {
 	DBPASSWORD: '100AppleJuice',
 	DBHOST: 'ds062448.mlab.com:62448',
 	DBNAME: 'tattoo-me',
-	SERVERNAME: 'dev-server'
+	SERVERNAME: 'dev-server',
+	cloud_name: 'tattoo-me',
+	api_key: '721159567314847',
+	api_secret: '0Pwe5bkv0figlWHZGPW8Nw07xTI',
+
 }
 
 // MAPS env TO ACTUAL ENVIRONMENT
 Object.keys(env).forEach(v => {
 	process.env[v] = process.env[v] || env[v]
-}) 
+})
 
+//Cloudinary Url
+env.CLOUDINARY = `CLOUDINARY_URL=cloudinary://${env.api_key}:${env.api_secret}@${env.cloud_name}`
 
 // MongoDb Connection String Builder
 env.CONNECTIONSTRING = `${env.DBPROTOCOL}://${env.DBUSERNAME}:${env.DBPASSWORD}@${env.DBHOST}/${env.DBNAME}`
