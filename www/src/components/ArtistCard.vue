9d08789b8691ffa70240ab0298ade6922dc07c79
Trever Johnston <treverj1029@gmail.com>
2017-09-13 16:21:19 -0600

    top artists page exists- currently draws just artist id and likes

---
+++ www/src/components/ArtistCard.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="artistcard">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2>
+                        {{artist._id}}
+                    </h2>
+                    <h2>
+                        {{artist.numWeeklyLikes}}
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'artistcard',
+        props: ["artist"],
+        data() {
+            return {
+            }
+        },
+        methods: {
+        },
+        computed: {
+        },
+
+        components: {
+        },
+        mounted() {
+        }
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    button {
+        margin-top: 2rem;
+    }
+
+    .viewartist {
+        padding-bottom: 15rem;
+    }
+</style>
