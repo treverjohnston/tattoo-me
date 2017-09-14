991787c535e0a4b3787c373cce8eed8b288804fb
Trever Johnston <treverj1029@gmail.com>
2017-09-14 11:17:12 -0600

    can now reach artist via home page

--- www/src/components/Card.vue
+++ www/src/components/Card.vue
@@ -52,11 +52,11 @@
 				<div class="row">
 					<div class="col-xs-12">
 						<div class="artist">
-							<!-- <router-link :to="'/viewartist'">
-								<button class="btn btn-default"> -->
+							<router-link :to="`/viewartist/${cardProp.creatorId}`">
+								<button class="btn btn-default">
 									<h3>Design by: {{cardProp.artistName}}</h3>
-								<!-- </button>
-							</router-link> -->
+								</button>
+							</router-link>
 						</div>
 					</div>
 				</div>
