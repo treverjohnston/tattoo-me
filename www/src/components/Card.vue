8bd0ad8bb2cb754f6f6d04dfbc90df393b4e2caa
Trever Johnston <treverj1029@gmail.com>
2017-09-15 18:09:57 -0600

    got rid of snackbar

--- www/src/components/Card.vue
+++ www/src/components/Card.vue
@@ -64,12 +64,12 @@
 					</div>
 				</div>
 			</div>
-			<md-snackbar :md-position="vertical + ' ' + horizontal" ref="snackbar" :md-duration="duration">
+			<!-- <md-snackbar :md-position="vertical + ' ' + horizontal" ref="snackbar" :md-duration="duration">
 				<span>Image Added To Camera Queue</span>
 				<router-link :to="`/camera`">
 					<md-button class="md-primary" @click="$refs.snackbar.close()">Camera</md-button>
 				</router-link>
-			</md-snackbar>
+			</md-snackbar> -->
 		</div>
 	</div>
 </template>
@@ -125,7 +125,6 @@
 			},
 			addToQueue(tat) {
 				this.$store.commit('addToQueue', tat)
-				this.$refs.snackbar.open()
 			},
 			confirm(card) {
 				this.$store.commit('confirm', card)
